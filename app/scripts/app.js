33998158aea4c4e7bf852eda81014aa06f8bcbaf
William Patton <will@pattonwebz.com>
2017-02-19 15:55:36 +0000

    function to handle swapping 'active' class on nav between pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,3 +33,29 @@ angular
         redirectTo: '/'
 	});
 });
+function hashchanged(){
+	var hash = location.hash;
+	//your code
+	console.log(hash);
+	if('' !== hash){
+		$('ul.nav.navbar-nav li').each( function () {
+			var link = $('a', this).attr('href');
+			link = link.substring(1, link.length);
+			if( hash === link && false === $( this ).hasClass( 'active' ) ){
+				$( this ).addClass( 'active' );
+			} else {
+				$( this ).removeClass( 'active' );
+			}
+		});
+	}
+}
+$( document ).ready( function() {
+	$('#exampleModal').on('show.bs.modal', function () {
+		var modal = $(this);
+  		modal.find('.modal-title').text('New User Info');
+	});
+	// Bind the event.
+	$(window).on('hashchange', function () {
+	    hashchanged();
+	}).trigger('hashchange');
+});
