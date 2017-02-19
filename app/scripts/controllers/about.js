780e32ad5cdc535959b5d4d9294ff98f945a9d5c
William Patton <will@pattonwebz.com>
2017-02-19 15:53:03 +0000

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name towerGameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the towerGameApp
+ */
+angular.module('towerGameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name towerGameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the towerGameApp
+ */
+angular.module('towerGameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
