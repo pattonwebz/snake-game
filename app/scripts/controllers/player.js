319e6f006d0dc9538dfbd240b4104b73e836cd47
William Patton <will@pattonwebz.com>
2017-02-20 07:42:18 +0000

    Add a position to the player and set/get functions

--- app/scripts/controllers/player.js
+++ app/scripts/controllers/player.js
@@ -8,33 +8,43 @@
  * Controller of the towerGameApp
  */
 angular.module('towerGameApp')
-	.controller('PlayerCtrl', ['$scope', 'playerFactory', function ($scope, playerFactory) {
+	.controller('PlayerCtrl', ['$scope', 'PlayerFactory', function ($scope, PlayerFactory) {
     	this.awesomeThings = [
     		'HTML5 Boilerplate',
     		'AngularJS',
     		'Karma'
     	];
-		$scope.message = playerFactory.getName();
+		$scope.message = PlayerFactory.getName();
 	}])
-	.factory('playerFactory', function() {
+	.factory('PlayerFactory', function() {
 
 		var service = {};
 		var _name = 'Player';
 		var _score = '0';
+		var _pos = {
+			x: 0,
+			y: 0
+		};
 
-			service.setName = function (name) {
-				_name = name;
-			};
-			service.getName = function () {
-				return _name;
-			};
+		service.setName = function (name) {
+			_name = name;
+		};
+		service.getName = function () {
+			return _name;
+		};
 
-			service.setScore = function (score) {
-		  		_score = score;
-			};
-			service.getScore = function () {
-		  		return _score;
-			};
+		service.setScore = function (score) {
+	  		_score = score;
+		};
+		service.getScore = function () {
+	  		return _score;
+		};
+		service.setPos = function (pos) {
+			_pos = pos;
+		};
+		service.getPos = function () {
+			return _pos;
+		};
 
 		return service;
 
